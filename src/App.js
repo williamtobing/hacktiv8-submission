1d627375e5036d986e9c8cebcd2f517dd7d9afea
William Lumbantobing <william.sl.tobing@gmail.com>
2021-09-12 21:38:39 +0700

    add some animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
+import sal from "sal.js";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 
 function App() {
+  useEffect(() => {
+    sal();
+  });
+
   return (
     <>
       <Navbar />
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,8 +4,20 @@ import { ClipboardListIcon } from "@heroicons/react/outline";
 function Home() {
   return (
     <section className="h-screen centered flex-col">
-      <ClipboardListIcon className="h-16 w-16 mb-4 dark:text-white" />
-      <h1 className="centered text-4xl font-bold dark:text-white">
+      <ClipboardListIcon
+        className="h-16 w-16 mb-4 dark:text-white"
+        data-sal="slide-up"
+        data-sal-duration="800"
+        data-sal-delay="800"
+        data-sal-easing="ease-out"
+      />
+      <h1
+        className="centered text-4xl font-bold dark:text-white"
+        data-sal="slide-right"
+        data-sal-duration="800"
+        data-sal-delay="800"
+        data-sal-easing="ease-out"
+      >
         Welcome To My Simple Portfolio
       </h1>
     </section>
