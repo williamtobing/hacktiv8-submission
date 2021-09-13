c2a633881f7d095f72e1d3978061d4970da22a40
William Lumbantobing <william.sl.tobing@gmail.com>
2021-09-13 14:15:34 +0700

    react router test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import sal from "sal.js";
+
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Blog from "./components/Blog/Blog";
+import Team from "./components/Team/Team";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   useEffect(() => {
     sal();
-  });
+  }, []);
 
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
-    </>
+      <Route path="/" render={Home} exact />
+      <Route path="/about" component={About} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/team" component={Team} />
+      <Route path="/contact" component={Contact} />
+    </Router>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { MoonIcon, SunIcon } from "@heroicons/react/outline";
 import "./Navbar.css";
 
@@ -54,12 +55,24 @@ const Navbar = () => {
           </span>
         </div>
         <ul className="flex py-4 select-none">
-          <li className="nav-li">Home</li>
-          <li className="nav-li">About</li>
-          <li className="nav-li">Portfolio</li>
-          <li className="nav-li">Blog</li>
-          <li className="nav-li">Team</li>
-          <li className="nav-li">Contact</li>
+          <li className="nav-li">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-li">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="nav-li">
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li className="nav-li">
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li className="nav-li">
+            <Link to="/team">Team</Link>
+          </li>
+          <li className="nav-li">
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
       </div>
     </nav>
